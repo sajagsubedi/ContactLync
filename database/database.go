3676485a5ac18821550fefc6fea52360ce6949c7
Sajag Subedi <sajagsubedi03@gmail.com>
2024-04-24 18:28:31 +0545

    All completed

--- database/database.go
+++ database/database.go
@@ -2,8 +2,10 @@ package database
 
 import(
   "os"
+  "fmt"
   "log"
   "time"
+  "regexp"
   "context"
   "go.mongodb.org/mongo-driver/bson"
   "go.mongodb.org/mongo-driver/bson/primitive"
@@ -11,14 +13,19 @@ import(
   "github.com/sajagsubedi/ContactLync/graph/model"
   "go.mongodb.org/mongo-driver/mongo/options"
   "go.mongodb.org/mongo-driver/mongo/readpref"
+  "github.com/joho/godotenv"
 )
-var connectionString string = os.Getenv("MONGO_URI")
 
 type DB struct {
   client *mongo.Client
 }
 
 func ConnectDb() *DB {
+  err:= godotenv.Load(".env")
+  if err != nil {
+    log.Fatal("Error on loading .env file")
+  }
+  connectionString:= os.Getenv("MONGO_URI")
   client,
   err:= mongo.NewClient(options.Client().ApplyURI(connectionString))
   if err != nil {
@@ -45,23 +52,69 @@ func ConnectDb() *DB {
 
 //methods in db
 func(db *DB) CreateUser(input *model.CreateUserInput) *model.User {
- userCollec:= db.client.Database("contactlync").Collection("users")
+  userCollec:= db.client.Database("contactlync").Collection("users")
   ctx,
   cancel:= context.WithTimeout(context.Background(), 20*time.Second)
   defer cancel()
-  inserg, err := userCollec.InsertOne(ctx, bson.M{"name": input.Name,  "phone": input.Phone, "address": input.Address,"email": input.Email, "relation": input.Relation,})
+  inserg,
+  err:= userCollec.InsertOne(ctx, bson.M {
+    "name": input.Name, "phone": input.Phone, "address": input.Address, "email": input.Email, "relation": input.Relation,
+  })
 
-	if err != nil {
-		log.Fatal(err)
-	}
+  if err != nil {
+    log.Fatal(err)
+  }
 
-	insertedID := inserg.InsertedID.(primitive.ObjectID).Hex()
-	newUser := model.User{ID: insertedID, Name: input.Name,  Phone: input.Phone, Address: input.Address,Email: input.Email, Relation: input.Relation,}
-	return &newUser
+  insertedID:= inserg.InsertedID.(primitive.ObjectID).Hex()
+  newUser:= model.User {
+    ID: insertedID,
+    Name: input.Name,
+    Phone: input.Phone,
+    Address: input.Address,
+    Email: input.Email,
+    Relation: input.Relation,
+  }
+  return &newUser
 }
 
 func(db *DB) UpdateUser(input *model.UpdateUserInput)*model.User {
-  return &model.User {}
+  userCollec:= db.client.Database("contactlync").Collection("users")
+  ctx,
+  cancel:= context.WithTimeout(context.Background(), 20*time.Second)
+  defer cancel()
+  updateList:= bson.M {}
+
+  if input.Name != nil {
+    updateList["name"] = input.Name
+  }
+
+  if input.Email != nil {
+    updateList["email"] = input.Email
+  }
+  if input.Phone != nil {
+    updateList["phone"] = input.Phone
+  }
+  if input.Address != nil {
+    updateList["address"] = input.Address
+  }
+  if input.Relation != nil {
+    updateList["relation"] = input.Relation
+  }
+  _id,
+  _:= primitive.ObjectIDFromHex(input.ID)
+
+  filter:= bson.M {
+    "_id": _id,
+  }
+  updateObj:= bson.M {
+    "$set": updateList,
+  }
+  results:= userCollec.FindOneAndUpdate(ctx, filter, updateObj, options.FindOneAndUpdate().SetReturnDocument(1))
+  var updatedUser model.User
+  if err:= results.Decode(updatedUser); err != nil {
+    log.Fatal(err)
+  }
+  return &updatedUser
 }
 
 func(db *DB) DeleteUser(id string) *model.DeleteUserResponse {
@@ -75,11 +128,14 @@ func(db *DB) DeleteUser(id string) *model.DeleteUserResponse {
     "_id": _id,
   }
 
-  _,err:= userCollec.DeleteOne(ctx, filter)
+  _,
+  err:= userCollec.DeleteOne(ctx, filter)
   if err != nil {
     log.Fatal(err)
   }
-  return &model.DeleteUserResponse{DeletedUserID:id}
+  return &model.DeleteUserResponse {
+    DeletedUserID: id,
+  }
 
 }
 
@@ -122,5 +178,33 @@ func(db *DB) User(id string) *model.User {
 }
 
 func(db *DB) UserByFilter(input *model.FilterInput) []*model.User {
-  return []*model.User {}
+  userCollec:= db.client.Database("contactlync").Collection("users")
+  ctx,
+  cancel:= context.WithTimeout(context.Background(), 20*time.Second)
+  defer cancel()
+  regexPattern:= fmt.Sprintf(".*%s.*", input.Value)
+  pattern,
+  err:= regexp.Compile(regexPattern)
+  if err != nil {
+    log.Fatal(err)
+  }
+  filter:= bson.M {
+    input.Field: bson.M {
+      "$regex": primitive.Regex {
+        Pattern: pattern.String(),
+        Options: "i",
+      }},
+  }
+
+  var users []*model.User
+  cursor,
+  err:= userCollec.Find(ctx, filter)
+  if err != nil {
+    log.Fatal(err)
+  }
+  if err = cursor.All(context.TODO(), &users); err != nil {
+    log.Fatal(err)
+
+  }
+  return users
 }
